01cb45a5a709610f05420f79b21b6c4f691e4ac8
James C. Tucker <hellojamestucker@gmail.com>
2022-08-28 11:45:33 -0500

    move to folders and update setup with apps

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,7 +1,29 @@
 #!/bin/sh
+
+# Greeting from program
 echo "---------------------------------------------------------"
 echo "$(tput setaf 2) Artemis: Hello there, explorer. I am powering up and will begin configuration checks momentarily.$(tput sgr 0)"
 echo "---------------------------------------------------------"
+
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+############################################
+# Install xcode, homebrew, nvm, etc.       #
+############################################
+
+# install xcode developer tools
+echo "---------------------------------------------------------"
+echo "$(tput setaf 2)Artemis: Installing xcode developer tools.$(tput sgr 0)"
+echo "---------------------------------------------------------"
+xcode-select --install
 
 # check if homebrew is installed, and install it if it's not
 echo "---------------------------------------------------------"
@@ -15,7 +37,7 @@ then
     echo "---------------------------------------------------------"
 else
     echo "---------------------------------------------------------"
-    echo "$(tput setaf 3)Artemis: Installing Homebrew. Homebrew requires osx command lines tools, please download xcode first$(tput sgr 0)"
+    echo "$(tput setaf 3)Artemis: Installing Homebrew.$(tput sgr 0)"
     echo "---------------------------------------------------------"
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
@@ -29,10 +51,6 @@ packages=(
     "git"
     "nvm"
     # "tmux"
-    # "neovim"
-    # "ripgrep"
-    # "fzf"
-    # "z"
     "yarn"
 )
 
@@ -100,3 +118,50 @@ else
     echo "---------------------------------------------------------"
     curl -sSL https://get.rvm.io | bash -s stable --ruby
 fi
+
+# Begin installing commonly used apps
+echo "---------------------------------------------------------"
+echo "$(tput setaf 2)Artemis: Installing your frequently used apps.$(tput sgr 0)"
+echo "---------------------------------------------------------"
+
+brew install --cask google-chrome
+brew install --cask firefox
+brew install --cask slack
+brew install --cask visual-studio-code
+# terminal emulator written in Rust
+brew install --cask warp
+# postgres explorer
+brew install --cask postico
+# docker 
+brew install --cask docker
+# markdown notes/second brain system
+brew install --cask obsidian
+# task management app
+brew install --cask sunsama
+# idea/thought inbox
+brew install --cask todoist
+# focus/distraction-blocking software (enable deep work)
+brew install --cask focus
+
+# install oh-my-zsh
+echo "---------------------------------------------------------"
+echo "$(tput setaf 2)Artemis: Installing oh-my-zsh.$(tput sgr 0)"
+echo "---------------------------------------------------------"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# install spaceship prompt
+echo "---------------------------------------------------------"
+echo "$(tput setaf 2)Artemis: Installing spaceship prompt.$(tput sgr 0)"
+echo "---------------------------------------------------------"
+
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+# zsh plugins
+echo "---------------------------------------------------------"
+echo "$(tput setaf 2)Artemis: Installing zsh plugins.$(tput sgr 0)"
+echo "---------------------------------------------------------"
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
